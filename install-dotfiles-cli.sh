051f6f187503e9aa9999bcb6fc63a39b13005c4d
Danilo <danilobjr@gmail.com>
2016-10-04 00:54:30 -0300

    CLI is installing correctly

--- install-dotfiles-cli.sh
+++ install-dotfiles-cli.sh
@@ -1,5 +1,9 @@
-export DOTFILES=~/.dotfiles
+export DOTFILES=$PWD/bin
+
+ln -s $DOTFILES/dotfiles.sh ~/dotfiles
 
 # add binaries into the path
-[[ -d $DOTFILES/bin ]] && PATH=$PATH:$DOTFILES/bin
+[ -n "$DOTFILES" ] && PATH=$PATH:$DOTFILES
 export PATH
+
+echo "dotfiles CLI installed. Now you can run 'dotfiles --help'"
