1bfdfda65ed28a81f8103b87eeb14faf27f61dda
Danilo Barros <danilobjr@gmail.com>
2017-02-03 15:39:32 -0200

    Update Microsoft.PowerShell_profile.ps1

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,7 @@
 # =======
 
 Import-Module git
+Import-Module psake
 
 # start
 # =====
@@ -15,6 +16,8 @@ New-Alias reload! Reload-Profile
 New-Alias repos Goto-Repos
 New-Alias .. Up-FolderLevel
 New-Alias ~ Goto-HomeFolder
+New-Alias psprofile Goto-PSProfile
+New-Alias modulesfolder Goto-ModulesFolder
 
 # colors
 # ======
@@ -26,6 +29,8 @@ New-Alias ~ Goto-HomeFolder
 # =========
 
 function Reload-Profile { & $profile }
-function Goto-Repos { cd C:\dev\repos }
-function Up-FolderLevel { cd .. } 
-function Goto-HomeFolder { cd $home } 
+function Goto-Repos { & Set-Location C:\dev\repos }
+function Up-FolderLevel { & Set-Location .. } 
+function Goto-HomeFolder { & Set-Location $home } 
+function Goto-PSProfile { & Invoke-Item "$($env:PSModulePath.split(';')[0].replace('\Modules', ''))\Microsoft.PowerShell_profile.ps1" } 
+function Goto-ModulesFolder { & Invoke-Item $env:PSModulePath.split(';')[0] } 
