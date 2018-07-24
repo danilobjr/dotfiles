eacca61ebc690fab8223c38b2408961050262d60
Danilo Barros <danilobjr@gmail.com>
2018-07-24 20:28:20 -0300

    Update and rename install-dotfiles-cli.sh to install.sh

--- install-dotfiles-cli.sh
+++
@@ -1,9 +0,0 @@
-export DOTFILES=$PWD/bin
-
-ln -s $DOTFILES/dotfiles.sh ~/dotfiles
-
-# add binaries into the path
-[ -n "$DOTFILES" ] && PATH=$PATH:$DOTFILES
-export PATH
-
-echo "dotfiles CLI installed. Now you can run 'dotfiles --help'"
