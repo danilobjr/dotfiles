f3505939b3a16854f697c9cb728d4890e1933460
Danilo Barros <danilobjr@gmail.com>
2018-07-24 21:23:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-wget -P ~/ZZZ/ https://github.com/danilobjr/dotfiles/archive/master.zip
-unzip ~/ZZZ/master.zip
-rm ~/ZZZ/master.zip
-mv ~/ZZZ/dotfiles-master/ ~/ZZZ/dotfiles/
+wget -P ~/ZZZ/ https://github.com/danilobjr/dotfiles/archive/master.zip;
+unzip ~/ZZZ/master.zip;
+rm ~/ZZZ/master.zip;
+mv ~/ZZZ/dotfiles-master/ ~/ZZZ/dotfiles/;
 
 #echo "Updating apt packages"
 
