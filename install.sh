9ff176c3c56ca3de644701822919670dbf1ae657
Danilo Barros <danilobjr@gmail.com>
2018-07-24 21:34:08 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ echo
 echo "Updating apt packages"
 echo
 
-sudo apt update
+sudo apt update;
 
 echo
 echo "Installing Git"
 echo
 
-sudo apt install git
+sudo apt install git;
 
 echo
 echo "Cloning dotfiles repo in /home directory"
 echo
 
-git clone https://github.com/danilobjr/dotfiles.git
+git clone https://github.com/danilobjr/dotfiles.git ZZZ;
