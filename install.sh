4d9fd2ad24194781ef0a867b52c8bef47424eae8
Danilo Barros <danilobjr@gmail.com>
2018-07-26 13:07:39 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,11 +54,11 @@ echoSectionTitle "############"
 
 echoSubSectionTitle "Installing Zsh"
 
-sudo apt install zsh;
+sudo apt install -y zsh;
 
 echoSubSectionTitle "Installing Git"
 
-sudo apt install git;
+sudo apt install -y git;
 
 echoSubSectionTitle "Installing Oh-My-Zsh"
 
@@ -77,23 +77,23 @@ echoSectionTitle "#######################"
 
 echoSubSectionTitle "Installing i3wm"
 
-sudo apt install i3;
+sudo apt install -y i3;
 
 echoSubSectionTitle "Installing i3blocks"
 
-sudo apt install i3blocks;
+sudo apt install -y i3blocks;
 
 echoSubSectionTitle "Installing compton"
 
-sudo apt install compton;
+sudo apt install -y compton;
 
 echoSubSectionTitle "Installing feh"
 
-sudo apt install feh;
+sudo apt install -y feh;
 
 echoSubSectionTitle "Installing ranger"
 
-sudo apt install ranger;
+sudo apt install -y ranger;
 
 echo
 echoSectionTitle "#############"
@@ -102,11 +102,11 @@ echoSectionTitle "#############"
 
 echoSubSectionTitle "Installing Vim"
 
-sudo apt install vim;
+sudo apt install -y vim;
 
 echoSubSectionTitle "Installing curl"
 
-sudo apt install curl;
+sudo apt install -y curl;
 
 echoSubSectionTitle "Installing Vundle"
 
@@ -122,7 +122,7 @@ nvm install node;
 
 echoSubSectionTitle "Installing snapd"
 
-sudo apt install snapd;
+sudo apt install -y snapd;
 
 echoSubSectionTitle "Installing Visual Studio Code"
 
