5d7f1cb4891ffa3589fceb0db6ffee4409f78800
Danilo Barros <danilobjr@gmail.com>
2020-05-23 11:06:40 -0300

    add i3-gaps dependency

--- install.sh
+++ install.sh
@@ -103,7 +103,7 @@ echoSectionTitle "Installing i3-gaps dependencies";
 aptInstall libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev \
 libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev \
 libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev \
-libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake;
+libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev;
 
 echoSectionTitle "Installing apt-get dependencies";
 aptInstall software-properties-common;
