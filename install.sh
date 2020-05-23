88d2f68d4b93facb07d84e804547bd37fcf94b43
Danilo Barros <danilobjr@gmail.com>
2020-05-23 11:48:24 -0300

    update polybar dependencies

--- install.sh
+++ install.sh
@@ -115,9 +115,15 @@ echoSectionTitle "Installing PyWal dependencies";
 aptInstall python3-pip imagemagick;
 
 echoSectionTitle "Installing Polybar dependencies";
-aptInstall clang cmake libcairo2-dev libxcb-xkb-dev xcb-proto \
-libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libasound2-dev \
-libpulse-dev libiw-dev libmpdclient-dev libcurl4-openssl-dev python-xcbgen;
+aptInstall build-essential git cmake cmake-data pkg-config python3-sphinx \
+libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev \
+python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev;
+
+echoSectionTitle "Installing Polybar optional dependencies";
+aptInstall libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev \
+#these can conflict with i3-gaps
+#i3-wm libjsoncpp-dev \
+libpulse-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev;
 
 echoSectionTitle "Installing snapd";
 aptInstall snapd;
