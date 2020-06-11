8ee1dd1276c61e45170071bb667fd09aaa404745
Danilo Barros <danilobjr@gmail.com>
2020-06-10 21:06:17 -0300

    feat(polybar): add new custom brightness module

--- polybar/backlight.sh
+++ polybar/backlight.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 brightness=$(xrandr --prop --verbose | grep Brightness | awk '{ print $2 }')
-percentage=$(echo "$brightness * 100" | bc -l)
-percentage=$(echo "$percentage/1" | bc)
-echo "$percentage%"
+fill=$(echo "$brightness * 10" | bc -l)
+fill=$(echo "$fill/1" | bc)
+empty=$((9-$fill))
+
+fill=$(seq -s '' 0 $(($fill-1)) | sed 's/.//g')
+empty=$(seq -s '' 0 $empty | sed 's/.//g')
+
+echo "$fill%{F#4c566a}$empty%{F-}"
