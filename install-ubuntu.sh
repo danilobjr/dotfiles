57ae4ec07f363d7d6052268f73f0baddff687e06
Danilo <danilobjr@gmail.com>
2021-12-15 13:21:24 -0300

    add lazygit installation

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -127,6 +127,13 @@ aptInstall dconf-editor;
 echoSectionTitle "Installing Git";
 aptInstall git;
 
+echoSectionTitle "Installing Lazygit";
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+');
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz";
+sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit;
+lazygit --version;
+rm -rf lazygit.tar.gz;
+
 echoSectionTitle "Installing glu (flutter dependency)";
 pacmanSynchronize glu;
 
