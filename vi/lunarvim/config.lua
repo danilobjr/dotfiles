7c1b5746635e4f569fdf1faec98de820576dff6c
Danilo <danilobjr@gmail.com>
2022-03-25 01:24:17 -0300

    set key maps for indentation

--- vi/lunarvim/config.lua
+++ vi/lunarvim/config.lua
@@ -63,6 +63,9 @@ lvim.keys.normal_mode["<a-;>"] = "<c-w>l"  -- focus on right panel
 lvim.keys.normal_mode["<a-,>"] = "<cmd>BufferLineCyclePrev<cr>"
 lvim.keys.normal_mode["<a-.>"] = "<cmd>BufferLineCycleNext<cr>"
 
+lvim.keys.normal_mode[">"] = ">>"
+lvim.keys.normal_mode["<"] = "<<"
+
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 -- local _, actions = pcall(require, "telescope.actions")
