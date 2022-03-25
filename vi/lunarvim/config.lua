bf0528d072ea59395a7e0c8d2aad1b9b1d8ade95
Danilo <danilobjr@gmail.com>
2022-03-25 01:17:34 -0300

    set key maps for tab navigation

--- vi/lunarvim/config.lua
+++ vi/lunarvim/config.lua
@@ -59,6 +59,10 @@ lvim.keys.normal_mode["<a-k>"] = "<c-w>j"  -- focus on left panel
 lvim.keys.normal_mode["<a-l>"] = "<c-w>k"  -- focus on left panel
 lvim.keys.normal_mode["<a-;>"] = "<c-w>l"  -- focus on right panel
 
+-- tab navigation
+lvim.keys.normal_mode["<a-,>"] = "<cmd>BufferLineCyclePrev<cr>"
+lvim.keys.normal_mode["<a-.>"] = "<cmd>BufferLineCycleNext<cr>"
+
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 -- local _, actions = pcall(require, "telescope.actions")
