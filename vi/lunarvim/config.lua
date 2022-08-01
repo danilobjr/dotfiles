b7c34ef6f3386bd318d63a2baa7586f3a486e6bb
Danilo <danilobjr@gmail.com>
2022-08-01 15:01:16 -0300

    feat(lvim): map F4 to toggle nvimtree

--- vi/lunarvim/config.lua
+++ vi/lunarvim/config.lua
@@ -70,6 +70,9 @@ lvim.keys.normal_mode["<a->>"] = "<cmd>BufferLineMoveNext<cr>"
 lvim.keys.normal_mode[">"] = ">>"
 lvim.keys.normal_mode["<"] = "<<"
 
+-- nvimtree
+lvim.keys.normal_mode["<F4>"] = "<cmd>NvimTreeToggle<cr>"
+
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 -- local _, actions = pcall(require, "telescope.actions")
