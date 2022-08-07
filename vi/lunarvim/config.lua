16d39722f05a7861e48e03474bc71e09419b2234
Danilo Barros <danilobjr@gmail>
2022-08-07 18:39:59 -0300

    feat(lvim): open explorer through which_key

--- vi/lunarvim/config.lua
+++ vi/lunarvim/config.lua
@@ -72,9 +72,6 @@ lvim.keys.normal_mode["<a->>"] = "<cmd>BufferLineMoveNext<cr>"
 lvim.keys.normal_mode[">"] = ">>"
 lvim.keys.normal_mode["<"] = "<<"
 
--- nvimtree
-lvim.keys.normal_mode["<F4>"] = "<cmd>NvimTreeToggle<cr>"
-
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 local _, actions = pcall(require, "telescope.actions")
@@ -95,6 +92,11 @@ lvim.builtin.telescope.defaults.mappings = {
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["e"] = {
+  name = "+Editor",
+  e = { ":NvimTreeToggle<cr>", "Toggle File Explorer" },
+}
+
 lvim.builtin.which_key.mappings["t"] = {
   name = "+Trouble",
   r = { "<cmd>Trouble references<cr>", "References" },
