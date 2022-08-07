1f4d789a2f19e54bf5f2a46d47eadc321516d80f
Danilo Barros <danilobjr@gmail>
2022-08-07 18:37:57 -0300

    refactor(lvim): repositioning some settings

--- vi/lunarvim/config.lua
+++ vi/lunarvim/config.lua
@@ -75,9 +75,6 @@ lvim.keys.normal_mode["<"] = "<<"
 -- nvimtree
 lvim.keys.normal_mode["<F4>"] = "<cmd>NvimTreeToggle<cr>"
 
--- autotag plugin (https://github.com/windwp/nvim-ts-autotag/issues/64#issuecomment-1200083102)
-lvim.builtin.treesitter.autotag.enable = true
-
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
 -- local _, actions = pcall(require, "telescope.actions")
@@ -142,6 +139,8 @@ lvim.builtin.nvimtree.setup.view.mappings.list = {
   { key = { "\\" }, action = "vsplit" },
   { key = { "<CR>" }, action = "cd" },
 }
+-- autotag plugin (https://github.com/windwp/nvim-ts-autotag/issues/64#issuecomment-1200083102)
+lvim.builtin.treesitter.autotag.enable = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
