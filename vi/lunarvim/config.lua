51e6b412b1264381b09b031646daadabbe4a9ee1
Danilo Barros <danilobjr@gmail>
2022-08-07 18:39:17 -0300

    feat(lvim): remap navigation in telescope

--- vi/lunarvim/config.lua
+++ vi/lunarvim/config.lua
@@ -77,21 +77,21 @@ lvim.keys.normal_mode["<F4>"] = "<cmd>NvimTreeToggle<cr>"
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
--- local _, actions = pcall(require, "telescope.actions")
--- lvim.builtin.telescope.defaults.mappings = {
---   -- for input mode
---   i = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---     ["<C-n>"] = actions.cycle_history_next,
---     ["<C-p>"] = actions.cycle_history_prev,
---   },
---   -- for normal mode
---   n = {
---     ["<C-j>"] = actions.move_selection_next,
---     ["<C-k>"] = actions.move_selection_previous,
---   },
--- }
+local _, actions = pcall(require, "telescope.actions")
+lvim.builtin.telescope.defaults.mappings = {
+  -- for input mode
+  i = {
+    ["<C-k>"] = actions.move_selection_next,
+    ["<C-l>"] = actions.move_selection_previous,
+    ["<C-j>"] = actions.cycle_history_next,
+    ["<C-;>"] = actions.cycle_history_prev,
+  },
+  -- for normal mode
+  n = {
+    ["<C-k>"] = actions.move_selection_next,
+    ["<C-l>"] = actions.move_selection_previous,
+  },
+}
 
 -- Use which-key to add extra bindings with the leader-key prefix
 -- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
